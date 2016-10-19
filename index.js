a6ac3149c1b04c0585fbfbf3ca9471b63d565a25
Tulir Asokan <tulir@maunium.net>
2016-10-19 14:12:29 +0300

    Fix indentation

--- index.js
+++ index.js
@@ -22,98 +22,98 @@ let notifications = require("sdk/notifications");
 
 let store = new pass.PasswordStore()
 let prefs = {
-    PASSWORD_STORE_CLIP_TIME: 10,
-    PASSWORD_STORE_DIR: "/home/tulir/.password-store"
+	PASSWORD_STORE_CLIP_TIME: 10,
+	PASSWORD_STORE_DIR: "/home/tulir/.password-store"
 }
 
 let panel = require("sdk/panel").Panel({
-  contentURL: "./panel.html",
-  onHide: () => button.state('window', {checked: false}),
-  height: 250,
-  width: 300
+	contentURL: "./panel.html",
+	onHide: () => button.state('window', {checked: false}),
+	height: 250,
+	width: 300
 })
 
 let button = require("sdk/ui").ToggleButton({
-    id: "pass",
-    label: "Pass",
-    icon: "./icon.svg",
-    onClick: state => {
-        if (state.checked) {
-            panel.show({position: button})
-            if (JSON.stringify(store.store) === "{}") {
-                update()
-            }
-        }
-    }
+	id: "pass",
+	label: "Pass",
+	icon: "./icon.svg",
+	onClick: state => {
+		if (state.checked) {
+			panel.show({position: button})
+			if (JSON.stringify(store.store) === "{}") {
+				update()
+			}
+		}
+	}
 })
 
 function update() {
-    exec.runPass({PASSWORD_STORE_DIR: "/home/tulir/.password-store"}, ["ls"],
-        (status, data, err) => {
-        	store.parseFull(data.split("\n"))
-            panel.port.emit("pass.list", store.store)
-        }
-    )
+	exec.runPass({PASSWORD_STORE_DIR: "/home/tulir/.password-store"}, ["ls"],
+		(status, data, err) => {
+			store.parseFull(data.split("\n"))
+			panel.port.emit("pass.list", store.store)
+		}
+	)
 }
 
 panel.port.on("pass.search", query => {
-    let results = store.search(query)
-    if (results.length > 20) {
-        panel.port.emit("pass.search.toomanyresults")
-    } else if (results.length === 0) {
-        panel.port.emit("pass.search.noresults")
-    } else {
-        panel.port.emit("pass.search.results", results)
-    }
+	let results = store.search(query)
+	if (results.length > 20) {
+		panel.port.emit("pass.search.toomanyresults")
+	} else if (results.length === 0) {
+		panel.port.emit("pass.search.noresults")
+	} else {
+		panel.port.emit("pass.search.results", results)
+	}
 })
 
 panel.port.on("pass.update", update)
 
 panel.port.on("pass.getlist", path => {
-    panel.port.emit("pass.list", store.dynamicGet(path))
+	panel.port.emit("pass.list", store.dynamicGet(path))
 })
 
 panel.port.on("pass.action", (action, path, password) => {
-    switch(action) {
-    case "copy-password":
-        exec.runPass(prefs, ["show", "-c", path.concat([password]).join("/")],
-            (status, data, err) => {
-                console.info(status)
-                console.log(data)
-                console.error(err)
-                panel.port.emit("pass.action.done",
-                    "copy-password", path, password)
-                notifications.notify({
-                    title: "Password copied",
-                    text: "/" + path.concat([password]).join("/"),
-                });
-            }
-        )
-        break
-    case "copy-username":
-        exec.runPass(prefs, ["show", path.concat([password]).join("/")],
-            (status, data, err) => {
-                // Find the line starting with "Username: "
-                let username = data.split("\n").find(line => {
-                    if (line.toLowerCase().startsWith("username: ")) {
-                        return true
-                    }
-                })
-                // Cut the "Username: " prefix out
-                username = username.substr("username: ".length)
-                clipboard.set(username)
-                setTimeout(() => {
-                    clipboard.set("Lorem ipsum dolor sit amet")
-                    clipboard.set("")
-                }, prefs.PASSWORD_STORE_CLIP_TIME * 1000)
-                panel.port.emit("pass.action.done",
-                    "copy-username", path, password)
-                notifications.notify({
-                    title: "Username copied",
-                    text: "/" + path.concat([password]).join("/"),
-                });
-            }
-        )
-        break
-    }
+	switch(action) {
+	case "copy-password":
+		exec.runPass(prefs, ["show", "-c", path.concat([password]).join("/")],
+			(status, data, err) => {
+				console.info(status)
+				console.log(data)
+				console.error(err)
+				panel.port.emit("pass.action.done",
+					"copy-password", path, password)
+				notifications.notify({
+					title: "Password copied",
+					text: "/" + path.concat([password]).join("/"),
+				});
+			}
+		)
+		break
+	case "copy-username":
+		exec.runPass(prefs, ["show", path.concat([password]).join("/")],
+			(status, data, err) => {
+				// Find the line starting with "Username: "
+				let username = data.split("\n").find(line => {
+					if (line.toLowerCase().startsWith("username: ")) {
+						return true
+					}
+				})
+				// Cut the "Username: " prefix out
+				username = username.substr("username: ".length)
+				clipboard.set(username)
+				setTimeout(() => {
+					clipboard.set("Lorem ipsum dolor sit amet")
+					clipboard.set("")
+				}, prefs.PASSWORD_STORE_CLIP_TIME * 1000)
+				panel.port.emit("pass.action.done",
+					"copy-username", path, password)
+				notifications.notify({
+					title: "Username copied",
+					text: "/" + path.concat([password]).join("/"),
+				});
+			}
+		)
+		break
+	}
 })
--- pass/store.js
+++ pass/store.js
@@ -14,182 +14,182 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 class PasswordStore {
-    constructor() {
-        this.store = new PasswordDirectory()
-    }
+	constructor() {
+		this.store = new PasswordDirectory()
+	}
 
-    /**
-     * Get the section designated by the given array. If the section does not
-     * exist, it will be created.
-     *
-     * @param tree An array with the name of each section as a string.
-     */
-    dynamicGet(tree) {
-        let obj = this.store
-        for(let i = 0; i < tree.length; i++) {
-            if (!obj.hasOwnProperty(tree[i])) {
-                obj[tree[i]] = new PasswordDirectory()
-            }
-            obj = obj[tree[i]]
-        }
-        return obj
-    }
+	/**
+	 * Get the section designated by the given array. If the section does not
+	 * exist, it will be created.
+	 *
+	 * @param tree An array with the name of each section as a string.
+	 */
+	dynamicGet(tree) {
+		let obj = this.store
+		for(let i = 0; i < tree.length; i++) {
+			if (!obj.hasOwnProperty(tree[i])) {
+				obj[tree[i]] = new PasswordDirectory()
+			}
+			obj = obj[tree[i]]
+		}
+		return obj
+	}
 
-    /**
-     * Fix the top level object, since the tree always has "Password Store"
-     * as the first level.
-     */
-    fixTopLevel() {
-        if (this.store.hasOwnProperty("Password Store")) {
-            this.store = this.store["Password Store"]
-        }
-    }
+	/**
+	 * Fix the top level object, since the tree always has "Password Store"
+	 * as the first level.
+	 */
+	fixTopLevel() {
+		if (this.store.hasOwnProperty("Password Store")) {
+			this.store = this.store["Password Store"]
+		}
+	}
 
-    /**
-     * Parse a full Password Store tree.
-     *
-     * @param lines Each line of the Password Store tree in an array.
-     */
-    parseFull(lines) {
-        var tree = new PathTree()
-        for (let i = 0; i < lines.length; i++) {
-            let rawLine = lines[i].replace(/\\+\s/g, " ")
-            if (rawLine.length === 0) {
-                // Ignore empty lines, they're nasty
-                continue
-            }
-            // Get the actual content of the current line
-            let line = rawLine.substr(firstLetterInLineOfTree(rawLine))
+	/**
+	 * Parse a full Password Store tree.
+	 *
+	 * @param lines Each line of the Password Store tree in an array.
+	 */
+	parseFull(lines) {
+		var tree = new PathTree()
+		for (let i = 0; i < lines.length; i++) {
+			let rawLine = lines[i].replace(/\\+\s/g, " ")
+			if (rawLine.length === 0) {
+				// Ignore empty lines, they're nasty
+				continue
+			}
+			// Get the actual content of the current line
+			let line = rawLine.substr(firstLetterInLineOfTree(rawLine))
 
-            let nextDepth = 0
-            if (i + 1 < lines.length) {
-                // Each section in an ASCII tree is always four characters
-                // long, so this'll always return the right depth.
-                //
-                // If the next line is empty, the first letter is 0 so that
-                // won't cause problems either.
-                nextDepth = firstLetterInLineOfTree(lines[i+1]) / 4
-            }
+			let nextDepth = 0
+			if (i + 1 < lines.length) {
+				// Each section in an ASCII tree is always four characters
+				// long, so this'll always return the right depth.
+				//
+				// If the next line is empty, the first letter is 0 so that
+				// won't cause problems either.
+				nextDepth = firstLetterInLineOfTree(lines[i+1]) / 4
+			}
 
-            if (nextDepth === tree.depth() + 1) {
-                // If the depth of the next line is larger than the depth of
-                // this line, this line must be a directory.
-                tree.enter(line)
-            } else {
-                // If the depth of the next line is equal to or smaller than
-                // the depth of this line, this line must be a regular password.
-                this.dynamicGet(tree.raw()).addPassword(line)
-            }
+			if (nextDepth === tree.depth() + 1) {
+				// If the depth of the next line is larger than the depth of
+				// this line, this line must be a directory.
+				tree.enter(line)
+			} else {
+				// If the depth of the next line is equal to or smaller than
+				// the depth of this line, this line must be a regular password.
+				this.dynamicGet(tree.raw()).addPassword(line)
+			}
 
-            // Exit to the depth of the next line. This only changes things
-            // if the next line is at a lower depth than this line.
-            tree.exitToDepth(nextDepth)
-        }
-        this.fixTopLevel()
-    }
+			// Exit to the depth of the next line. This only changes things
+			// if the next line is at a lower depth than this line.
+			tree.exitToDepth(nextDepth)
+		}
+		this.fixTopLevel()
+	}
 
-    search(query) {
-        return this.store.search(query.toLowerCase(), new PathTree())
-    }
+	search(query) {
+		return this.store.search(query.toLowerCase(), new PathTree())
+	}
 }
 
 class PasswordDirectory {
-    addPassword(name) {
-        this[name] = ""
-    }
+	addPassword(name) {
+		this[name] = ""
+	}
 
-    search(query, tree) {
-        var results = []
-        for (let key in this) {
-            let newTree = tree.clone().enter(key)
-            if (typeof(this[key]) === "string") {
-                if (key.toLowerCase().indexOf(query) !== -1) {
-                    results.push({
-                        path: "/" + newTree.toString(),
-                        type: "password"
-                    })
-                }
-            } else if (newTree.toString().toLowerCase().indexOf(query) !== -1) {
-                results.push({
-                    path: "/" + newTree.toString() + "/",
-                    type: "directory"
-                })
-            } else {
-                results = results.concat(this[key].search(query, newTree))
-            }
-        }
-        return results
-    }
+	search(query, tree) {
+		var results = []
+		for (let key in this) {
+			let newTree = tree.clone().enter(key)
+			if (typeof(this[key]) === "string") {
+				if (key.toLowerCase().indexOf(query) !== -1) {
+					results.push({
+						path: "/" + newTree.toString(),
+						type: "password"
+					})
+				}
+			} else if (newTree.toString().toLowerCase().indexOf(query) !== -1) {
+				results.push({
+					path: "/" + newTree.toString() + "/",
+					type: "directory"
+				})
+			} else {
+				results = results.concat(this[key].search(query, newTree))
+			}
+		}
+		return results
+	}
 }
 
 class PathTree {
-    constructor(arr) {
-        if (arr === undefined) {
-            arr = []
-        } else {
-            arr = [].concat(arr)
-        }
-        this.tree = arr
-    }
+	constructor(arr) {
+		if (arr === undefined) {
+			arr = []
+		} else {
+			arr = [].concat(arr)
+		}
+		this.tree = arr
+	}
 
-    clone() {
-        return new PathTree(this.tree)
-    }
+	clone() {
+		return new PathTree(this.tree)
+	}
 
-    enter(section) {
-        this.tree[this.tree.length] = section
-        return this
-    }
+	enter(section) {
+		this.tree[this.tree.length] = section
+		return this
+	}
 
 
-    /**
-     * Exit out of the given number of sections.
-     *
-     * @param numOfSections The number of sections to exit out of.
-     */
-    exit(numOfSections) {
-        this.exitToDepth(this.tree.length - numOfSections)
-        return this
-    }
+	/**
+	 * Exit out of the given number of sections.
+	 *
+	 * @param numOfSections The number of sections to exit out of.
+	 */
+	exit(numOfSections) {
+		this.exitToDepth(this.tree.length - numOfSections)
+		return this
+	}
 
-    /**
-     * Exit to the given depth.
-     *
-     * @param depth The depth to exit to.
-     */
-    exitToDepth(depth) {
-        if (depth === undefined) {
-            depth = this.tree.length - 1
-        }
-        this.tree = this.tree.slice(0, depth)
-        return this
-    }
+	/**
+	 * Exit to the given depth.
+	 *
+	 * @param depth The depth to exit to.
+	 */
+	exitToDepth(depth) {
+		if (depth === undefined) {
+			depth = this.tree.length - 1
+		}
+		this.tree = this.tree.slice(0, depth)
+		return this
+	}
 
-    toString() {
-        return this.tree.join("/")
-    }
+	toString() {
+		return this.tree.join("/")
+	}
 
-    depth() {
-        return this.tree.length
-    }
+	depth() {
+		return this.tree.length
+	}
 
-    raw() {
-        return this.tree
-    }
+	raw() {
+		return this.tree
+	}
 }
 
 function firstLetterInLineOfTree(str) {
-    for(let i = 0; i < str.length; i++) {
-        switch(str.charAt(i)) {
-        case "|":
-        case "-":
-        case " ":
-        case "`":
-            continue
-        default:
-            return i
-        }
-    }
+	for(let i = 0; i < str.length; i++) {
+		switch(str.charAt(i)) {
+		case "|":
+		case "-":
+		case " ":
+		case "`":
+			continue
+		default:
+			return i
+		}
+	}
 }
 
 exports.PasswordStore = PasswordStore
