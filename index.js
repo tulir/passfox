5f7257ec63e50b9f10d03e405ea0d0a8a1822067
Tulir Asokan <tulir@maunium.net>
2016-10-25 22:40:02 +0300

    Remove unused imports

--- index.js
+++ index.js
@@ -13,7 +13,6 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-const self = require("sdk/self")
 const pass = require("pass/store")
 const exec = require("pass/exec")
 const config = require("pass/config")
